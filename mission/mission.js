781c34c50d135e89d19ae3be30fb661cf105d2fb
kaitlinheusner <kheusner@byui.edu>
2025-01-17 12:55:26 -0700

    final

--- mission/mission.js
+++ mission/mission.js
@@ -1,1 +1,17 @@
-thme = document.getElementById("theme").addEventListener("change")
+const themeSelector = document.querySelector("#theme")
+
+function changeTheme() {
+
+    const currentValue = themeSelector.value;
+
+    if (currentValue == "dark"){
+        document.body.classList.add("dark");
+        document.querySelector(".logo_img").src = "/mission/byui-logo_white.png"
+    }else{
+        document.body.classList.remove("dark");
+        document.querySelector(".logo_img").src = "/mission/byui-logo_blue.webp"
+    }
+
+}
+
+themeSelector.addEventListener("change", changeTheme)
