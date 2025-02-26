3dd73fdc452131695f1fc15c539aa109f780316b
kaitlinheusner <kheusner@byui.edu>
2025-02-26 12:14:25 -0700

    Ponder Completion

--- js_objects/courses.js
+++ js_objects/courses.js
@@ -3,16 +3,21 @@ const aCourse = {
     name: "Javascript Language",
     sections: [
         { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
-        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'},
     ],
-    enrollStudent: function(sectionNum){
-        const sectionIndex = this.sections.findIndex((section) => section.sectionNum == sectionNum);
-
-        if (sectionIndex > 0){
-            this.sections[sectionIndex].enrolled++;
+    changeEnrollment: function(sectionNum, add = true){
+        const sectionIndex = this.sections.findIndex(
+            (section) => section.sectionNum == sectionNum
+        );
+        if (sectionIndex >= 0) {
+            if (add) {
+                this.sections[sectionIndex].enrolled++;
+            } else{
+                this.sections[sectionIndex].enrolled--;
+            }
         }
         renderSections(this.sections);
-    },
+    }
   };
 
 function renderCourseDetails(course){
@@ -35,6 +40,16 @@ function renderSections(sections){
     //for each element in the array, use sectionTemplate function on it = map which will return it 
     document.querySelector("#sections").innerHTML = html.join("");
 }
+
+document.querySelector("#enrollStudent").addEventListener("click", function(){
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.changeEnrollment(sectionNum, true);
+})
+
+document.querySelector("#dropStudent").addEventListener("click", function(){
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.changeEnrollment(sectionNum, false);
+})
 
 renderCourseDetails(aCourse);
 renderSections(aCourse.sections);
