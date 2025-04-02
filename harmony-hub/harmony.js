56f64230a541d79daa387fe240c525e5623923ae
kaitlinheusner <kheusner@byui.edu>
2025-04-02 13:42:46 -0600

    Update

---
+++ harmony-hub/harmony.js
@@ -0,0 +1,46 @@
+import { successTemplate } from "./harmony_message.js";
+
+const menuButton = document.querySelector(".menu-button")
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize(){
+    const menu = document.querySelector(".menu");
+    if (window.innerWidth > 1000){
+        menu.classList.remove("hide");
+    } else{
+        menu.classList.add("hide");
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+
+function submitForm(event) {
+    event.preventDefault();
+    let userName = document.querySelector("input[name='name']").value;
+    let songName = document.querySelector("input[name='song']").value;
+    let artistInputs = document.querySelectorAll("input[name= 'artist']").value;
+    let artistName = Array.from(artistInputs).map(input => input.value).join(", ");
+
+    let info = {
+      name: userName,
+      song: songName,
+      artist: artistName,
+    };
+
+    let message = successTemplate(info)
+
+    document.getElementById("song-rec").classList.add("hide");
+    let summary = document.getElementById("summary");
+    summary.classList.remove("hide");
+    summary.innerHTML = message;
+  }
+
+  document.getElementById("song-rec").addEventListener("submit", submitForm);
---
+++ harmony-hub/harmony_message.js
@@ -0,0 +1,5 @@
+
+export function successTemplate(info){
+    return `
+    <p>Thank you, ${info.name} for sharing ${info.song} by ${info.artist}.</p>`
+}
